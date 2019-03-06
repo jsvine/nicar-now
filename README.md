8047e1dab02a34464c004daac21cfe7840abf805
Jeremy Singer-Vine <jsvine@gmail.com>
2019-03-06 08:47:16 -0500

    Update for NICAR 2019

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NICAR-NOW
 
-Your [__unofficial__ guide](https://jsvine.github.io/nicar-now/) to what's happening next at [NICAR 2018](https://ire.org/conferences/nicar18/).
+Your [__unofficial__ guide](https://jsvine.github.io/nicar-now/) to what's happening next at [NICAR 2019](https://www.ire.org/conferences/nicar-2019/).
 
-Based on data from [`jsvine/nicar-2018-schedule`](https://github.com/jsvine/nicar-2018-schedule).
+Based on data from [`jsvine/nicar-2019-schedule`](https://github.com/jsvine/nicar-2019-schedule).
--- docs/js/schedule.js
+++ docs/js/schedule.js
@@ -1,8 +1,19 @@
 (function () {
     var root = this;
 
-    var DATA_URL = "https://raw.githubusercontent.com/jsvine/nicar-2018-schedule/master/schedule/nicar-2018-schedule.json";
-    var TIME_OFFSET = -6;
+    var DATA_URL = "https://raw.githubusercontent.com/jsvine/nicar-2019-schedule/master/schedule/nicar-2019-schedule.json";
+    var TZ = "America/Los_Angeles";
+
+    var TZ_OFFSET = (function () {
+        var d = new Date();
+        var d_gmt = new Date(d.toLocaleString("en-US", {
+            timeZone: "GMT"
+        }));
+        var d_local = new Date(d.toLocaleString("en-US", {
+            timeZone: TZ
+        }));
+        return d_local - d_gmt;
+    })();
 
     var getJSON = function (url, callback) {
         var xhr = new XMLHttpRequest();
@@ -46,9 +57,11 @@
     var tmp_el = document.getElementById("session-template");
     var tmpl = _.template(tmp_el.innerHTML);
 
-    var timeToGmt = function (date, time, tz) {
-        var offset = 1000*3600*(tz||0); // -0500
-        var dt = new Date(new Date(date + "T" + time + "Z")*1 - offset);
+    var confTime2DateObj = function (date, time) {
+        // var offset_hrs = (TZ_OFFSET/(1000 * 3600));
+        //var date_str = date + "T" + time + (offset_hrs >= 0 ? "+" : "") + offset_hrs; 
+        var wo_tz = new Date(date + "T" + time + "+00:00");
+        var dt = new Date(wo_tz.getTime() - TZ_OFFSET);
         return dt;
     };
 
@@ -56,10 +69,9 @@
         var el = createElement("div");
         el.innerHTML = tmpl(session);
         var session_el = el.children[0];
-        var dt = timeToGmt(
+        var dt = confTime2DateObj(
             session["date"],
             session["time_start"],
-            TIME_OFFSET
         ).toISOString();
         session_el.setAttribute("data-dt", dt);
         session_el.setAttribute("data-date", session["date"]);
@@ -67,19 +79,19 @@
     };
 
     var DATES = {
-        "2018-03-07": "Weds., March 7",
-        "2018-03-08": "Thurs., March 8",
-        "2018-03-09": "Fri., March 9",
-        "2018-03-10": "Sat., March 10",
-        "2018-03-11": "Sun., March 11"
+        "2019-03-06": "Weds., March 6",
+        "2019-03-07": "Thurs., March 7",
+        "2019-03-08": "Fri., March 8",
+        "2019-03-09": "Sat., March 9",
+        "2019-03-10": "Sun., March 10",
     };
 
     var SHORT_DATES = {
-        "2018-03-07": "Wednesday",
-        "2018-03-08": "Thursday",
-        "2018-03-09": "Friday",
-        "2018-03-10": "Saturday",
-        "2018-03-11": "Sunday"
+        "2019-03-06": "Wednesday",
+        "2019-03-07": "Thursday",
+        "2019-03-08": "Friday",
+        "2019-03-09": "Saturday",
+        "2019-03-10": "Sunday",
     };
 
     var SHORT_DESC_LEN = 300;
@@ -98,11 +110,11 @@
             var times = _.groupBy(days[date], "time_start");
             var option = createElement("option");
             option.value = date;
-            option.innerHTML = SHORT_DATES[date] + " @ anytime";
+            option.innerHTML = SHORT_DATES[date] + ", all sessions";
             optgroup.appendChild(option);
             _.keys(times).forEach(function (time) {
                 var option = createElement("option", "specific-time-slot");
-                option.value = timeToGmt(date, time, TIME_OFFSET).toISOString();
+                option.value = confTime2DateObj(date, time).toISOString();
                 option.innerHTML = SHORT_DATES[date] + " @ " + time;
                 optgroup.appendChild(option);
             });
@@ -120,7 +132,7 @@
                 session["title"]
             ].join("|"); 
             session["full_date"] = DATES[session["date"]];
-            session["description"] = session["description"].replace(/\n/g, "<br/>") || "[No description yet.]";
+            session["description"] = (session["description"] || "").replace(/\n/g, "<br/>") || "[No description yet.]";
         });
         data.sort(function (a, b) {
             return a["sort_key"].localeCompare(b["sort_key"]);
